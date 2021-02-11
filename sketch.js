711a7c242983b33112fe8b063023cdad014ab651
tharu <cbhuvaneswari9@gmail.com>
2021-02-11 16:57:01 +0530

    new platform created

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+    platform1 = new Ground (200, 305, 300, 170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -61,4 +61,4 @@ function draw(){
 
     bird.display();
     platform.display();
-}
+}   
